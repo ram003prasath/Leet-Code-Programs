dea8064bbecf7a3d5bf95b4ac1e9ec197a353e91
ram <raampraasaath@gmail.com>
2024-08-10 14:24:43 +0530

    Added removeElement.java

---
+++ removeElement.java
@@ -0,0 +1,14 @@
+public class removeElement {
+    public int deleteElement(int[] nums, int val) {
+
+        int k = 0;
+        for (int i=0; i<nums.length; i++) {
+            if(nums[i] != val){
+                nums[k++] = nums[i];
+            }
+        }
+
+        return k;
+    }
+
+}
