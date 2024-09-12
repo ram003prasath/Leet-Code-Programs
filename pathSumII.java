18fcba971e612a674e94052fef5b6ba6816db43d
ram <raampraasaath@gmail.com>
2024-09-12 13:26:49 +0530

    path sum

---
+++ pathSumII.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class pathSumII {
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        if(root == null){
+            return result;
+        }
+
+        findPath(root, targetSum, new ArrayList<>());
+        return result;
+
+    }
+
+    public void findPath(TreeNode root, int targetSum, List<Integer> lst){
+        if(root == null){
+            return;
+        }
+        lst.add(root.val);
+        if(root.left == null && root.right == null && root.val == targetSum){
+            result.add(new ArrayList<>(lst));
+        }
+        findPath(root.left, targetSum - root.val, new ArrayList<>(lst));
+        findPath(root.right, targetSum - root.val, new ArrayList<>(lst));
+    }
+}
