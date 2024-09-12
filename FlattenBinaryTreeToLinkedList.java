e9fe8532264206de6ee9c4231521c712d0dc2a70
ram <raampraasaath@gmail.com>
2024-09-12 14:07:13 +0530

    flatten BTree

---
+++ FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,12 @@
+public class FlattenBinaryTreeToLinkedList {
+    TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        if(root == null){ return; }
+        flatten(root.left);
+        flatten(root.right);
+        root.right = prev;
+        root.right = null;
+        prev = root;
+    }
+}
+
